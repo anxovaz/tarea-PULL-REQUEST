7c545cabf482723511a742956e610a5c6b3d852e
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-02-03 13:29:35 +0100

    Add boletín 11

---
+++ boletin11.py
@@ -0,0 +1,23 @@
+#Ficheros boletín 11
+
+'''Ejercicio 1
+Crear un programa xestor de notas persoais, que permita ao usuario gardar e consultar notas.
+O usuario pode engadir unha nova nota (texto libre).
+
+
+As notas gárdanse nun ficheiro de texto (notas.txt), unha por liña.
+
+
+O programa pode listar todas as notas gardadas.
+
+
+O usuario pode buscar notas que conteñan unha palabra clave.
+'''
+
+def ejercicio1():
+    with open('./archivos_boletin11/ejercicio1.txt',"w") as f:
+        inputUsuario = input("Ingrese su nota: ")
+        f.write(inputUsuario)
+        f.close()
+
+ejercicio1()
